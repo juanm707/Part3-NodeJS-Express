8c40e50d804666dfe169121178199d745ebd2920
juanmartinez <juanm.jm49@gmail.com>
2020-06-23 23:24:53 -0700

    get post delete notes, used postman to check

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
 let notes = [
 	     {
 		 id: 1,
@@ -20,11 +23,61 @@ let notes = [
 	     }
 ];
 
-const app = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'application/json'});
-	res.end(JSON.stringify(notes)); 
+app.get('/', (req, res) => {
+	res.send('<h1>Hello World!</h1><p>This is a test</p>');
 });
 
+app.get('/api/notes', (req, res) => {
+	res.json(notes);
+});
+
+const generateID = () => {
+    const maxID = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+    return maxID + 1;
+}
+
+app.post('/api/notes', (req, res) => {
+	// console.log(req.headers);
+	const body = req.body;
+	console.log(req);
+
+	if (!body.content) {
+	    return res.status(400).json({
+		    error: 'content missing'
+	       	});
+	}
+
+	const note = {
+	    content: body.content,
+	    important: body.important || false,
+	    date: new Date(),
+	    id: generateID(),
+	}
+
+	notes = notes.concat(note);
+	console.log(note);
+	res.json(note);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+	const id = Number(req.params.id);
+	console.log(id, typeof id);
+	const note = notes.find(note => note.id === id);
+	if (note) {
+	    res.json(note);
+	} else {
+	    res.status(404).end();
+	}
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+	const id = Number(req.params.id);
+	notes = notes.filter(note => note.id !== id);
+	res.status(204).end();
+});
+
+
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
